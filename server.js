4c4d1df6def0da5ac2d262b04d25794c1a43315b
Branson Camp <bransoncamp@outlook.com>
2021-07-20 23:26:39 -0600

    Initial Commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const port = process.env.PORT || 80;
+
+
+
+// app.get('/', (request, response) => {
+//   response.send(`<html><head></head><body><p style="font-size: 500; font-family: sans-serif;">>:)</p></body></html>`)
+//   console.log(request.ip)
+
+// })
+
+app.use(express.static(__dirname + '/public'));
+
+http.listen(port, () => console.log('listening on port ' + port));
